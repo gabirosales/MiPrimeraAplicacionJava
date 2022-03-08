b870ccb66baf1aff65cc8771d943b0314c53b2ab
gabirosales <gabrielarosales169@gmail.com>
2022-03-08 11:43:31 -0600

    ConversionMonedas

--- src/miprimeraaplicacionjava/MonedaDePaises.java
+++
@@ -1,9 +0,0 @@
-package miprimeraaplicacionjava;
- 
-public class MonedaDePaises {
-     public static void main(String[] args){
-         
-         String nombre = "Estudiante Gabriela";
-     }
-    
-}
